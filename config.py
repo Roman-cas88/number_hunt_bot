93dcbfee55f7719fd12c80ae9ad00a890c6d3df8
Roman-cas88 <orekhovskiy.roman@gmail.com>
2026-09-01 18:47:25 +0200

    Fix: use /tmp for database on Railway

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))  # например, "123456789,987654321"
 
 # Путь к базе данных
-DB_PATH = "data/game.db"
+DB_PATH = "/tmp/game.db"
 
 # Настройки напоминаний
 REMINDER_INTERVAL_SECONDS = 3600  # 1 час
